edc693c8d0359b9cea5369d39a494a3ca72a8b68
Alexandra F <93889946+alexS317@users.noreply.github.com>
2023-07-03 20:54:26 +0200

    Add terrain, implement camera movement and zoom

--- FinalGame/Assets/Scripts/Player/CameraMovement.cs
+++ FinalGame/Assets/Scripts/Player/CameraMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraMovement : MonoBehaviour
 {
@@ -8,18 +9,47 @@ public class CameraMovement : MonoBehaviour
     [SerializeField] private float closestDistanceToPlayer;
 
     private Vector3 _previousPlayerPosition;
+    private float _maximumDistanceFromPlayer;
 
     // Start is called before the first frame update
     void Start()
     {
         // Player position in the first frame is the first previous position
         _previousPlayerPosition = playerFigure.transform.position;
+
+        // Initial camera position is the maximum distance to the player
+        _maximumDistanceFromPlayer = Mathf.Abs(Vector3.Distance(transform.position, _previousPlayerPosition));
     }
 
     // Update is called once per frame
     void Update()
     {
         FollowPlayer();
+    }
+
+    void OnCameraMovement(InputValue input)
+    {
+        Vector2 inputVector = input.Get<Vector2>();
+        var playerTransform = playerFigure.transform; // Get the player transform
+        transform.RotateAround(playerTransform.position, Vector3.up, inputVector.x); // Rotate around player
+    }
+
+    void OnCameraZoom(InputValue input)
+    {
+        Vector2 inputVector = input.Get<Vector2>();
+
+        var shiftValue = inputVector.y * 0.05f; // Multiply with 0.05 to make it less sensitive (smaller movements)
+        transform.Translate(new Vector3(0, 0, shiftValue)); // Move camera towards or away from player
+
+        // Distance between player and camera position
+        var distance = Mathf.Abs(Vector3.Distance(transform.position, playerFigure.transform.position));
+
+        // Don't zoom further if the camera is too close or too far away
+        if (distance < closestDistanceToPlayer || distance > _maximumDistanceFromPlayer)
+        {
+            // Invert the shift value so it won't zoom any further
+            transform.Translate(new Vector3(0, 0, -shiftValue));
+        }
     }
 
     // Camera follows the player
